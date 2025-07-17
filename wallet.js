43ea87663efd11562662b8f98d05d88f6587bbfd
hypra token <hypratoken@gmail.com>
2025-07-18 00:36:48 +0530

    Add files via upload

---
+++ wallet.js
@@ -0,0 +1,13 @@
+
+document.getElementById("walletBtn").addEventListener("click", async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        try {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            alert("Wallet connected: " + accounts[0]);
+        } catch (err) {
+            alert("Connection failed.");
+        }
+    } else {
+        alert("MetaMask not detected.");
+    }
+});
