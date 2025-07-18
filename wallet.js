9eeae184477be7d4fac587b4cc137bcfd7e90253
hypra token <hypratoken@gmail.com>
2025-07-18 13:26:31 +0530

    wallet.js

--- wallet.js
+++ wallet.js
@@ -1,13 +1,12 @@
-
 document.getElementById("walletBtn").addEventListener("click", async () => {
-    if (typeof window.ethereum !== 'undefined') {
-        try {
-            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-            alert("Wallet connected: " + accounts[0]);
-        } catch (err) {
-            alert("Connection failed.");
-        }
-    } else {
-        alert("MetaMask not detected.");
+  if (typeof window.ethereum !== 'undefined') {
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      alert("Wallet connected: " + accounts[0]);
+    } catch (err) {
+      alert("Connection failed.");
     }
+  } else {
+    alert("MetaMask not detected.");
+  }
 });
